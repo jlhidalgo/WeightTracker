74a4831cbf59610368f445de11608ec829f11cda
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2021-09-24 21:50:08 -0700

    fix datetime property

--- src/WeightTracker.ClassLib/Models/WeightRecord.cs
+++ src/WeightTracker.ClassLib/Models/WeightRecord.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace WeightTracker.ClassLib.Models
 {
 
     public class WeightRecord
     {
         public int Id { get; set; }
-        public datetime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
         public double Weight { get; set; }
         public double BodyFatPercent { get; set; }
         public double BonesPercent { get; set; }
